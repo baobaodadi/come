f5b4cd469957c2b554301b5c75e5f81d2835a6a1
shendadi <shendadi@sogou-inc.com>
2018-09-24 23:28:10 +0800

    css

--- src/apps/page/mail/index.jsx
+++ src/apps/page/mail/index.jsx
@@ -7,6 +7,9 @@ import {Link, withRouter} from "react-router-dom";
 import * as actionTypes from "../../../config/actionTypes";
 import {connect} from "react-redux";
 import './index.less';
+import mailimg from '../../../images/mail.png';
+import passwordimg from '../../../images/password.png';
+import confirmimg from '../../../images/confirm.png';
 import {
     Table,
     Input,
@@ -94,7 +97,10 @@ class Mail extends Component {
     validateToNextPassword(rule, value, callback) {
         const form = this.props.form;
         if (!/^(?![a-zA-Z]+$)(?![A-Z0-9]+$)(?![A-Z\W_]+$)(?![a-z0-9]+$)(?![a-z\W_]+$)(?![0-9\W_]+$)[a-zA-Z0-9\W_]{8,}$/.test(value)) {
-            callback('密码不符合规则');
+            callback( <div className="title">
+                <img src={passwordimg} alt=""/>
+                <span>设置邮箱密码</span>
+            </div>);
         }
         else if (form.getFieldValue('password').indexOf(form.getFieldValue('email')) !== -1) {
             callback('密码不能包含邮箱');
@@ -130,31 +136,49 @@ class Mail extends Component {
     render() {
         const {asset, mail} = this.props;
         const {getFieldDecorator} = this.props.form;
-        const suffix = <Icon type="eye" style={{color: this.state.eye ? 'blue' : '#dddddd'}} onClick={this.eye}/>;
+        const suffix = <Icon type="eye" theme="filled" style={{color: this.state.eye ? '#007FFF ' : '#B7BBC2'}} onClick={this.eye}/>;
 
         return (
 
             <div className="mail">
                 <div className="find">
                     <Form onSubmit={this.handleSubmit}>
+                        <div className='mailinfo'>
+                            <div className="title">
+                                <img src={mailimg} alt=""/>
+                                <span>邮箱信息</span>
+                            </div>
+                            <div className="formitem">
+                                <FormItem
+                                    hasFeedback
+                                    style={{'display': 'inline-block', 'width': '217px'}}
+                                >
+                                    {getFieldDecorator('email', {
+                                        rules: [{
+                                            required: true, message: '请输入邮箱',
+                                        },
+                                            {
+                                                validator: this.validateMail
+                                            }],
+                                    })(
+                                        <Input size="large"/>
+                                    )}
+                                </FormItem>
+                                <div className='zhui'>
+                                    @sogou-inc.com
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='passwordinfo'>
+                            <div className="title">
+                                <img src={passwordimg} alt=""/>
+                                <span>设置邮箱密码</span>
+                            </div>
+                        <div className="formitem">
                         <FormItem
-                            label="邮箱"
                             hasFeedback
-                        >
-                            {getFieldDecorator('email', {
-                                rules: [{
-                                    required: true, message: '请输入邮箱',
-                                },
-                                    {
-                                        validator: this.validateMail
-                                    }],
-                            })(
-                                <Input/>
-                            )}
-                        </FormItem>
-                        <FormItem
-                            label="密码"
-                            hasFeedback
+                            style={{'display': 'inline-block', 'width': '380px'}}
                         >
                             {getFieldDecorator('password', {
                                 rules: [{
@@ -163,12 +187,21 @@ class Mail extends Component {
                                     validator: this.validateToNextPassword,
                                 }],
                             })(
-                                <Input type={this.state.eye ? 'text' : 'password'} suffix={suffix}/>
+                                <Input size="large" type={this.state.eye ? 'text' : 'password'} suffix={suffix}/>
                             )}
                         </FormItem>
+                        </div>
+                        </div>
+
+                        <div className='confirminfo'>
+                            <div className="title">
+                                <img src={confirmimg} alt=""/>
+                                <span>确认密码</span>
+                            </div>
+                            <div className="formitem">
                         <FormItem
-                            label="确认密码"
                             hasFeedback
+                            style={{'display': 'inline-block', 'width': '380px'}}
                         >
                             {getFieldDecorator('confirm', {
                                 rules: [{
@@ -183,6 +216,8 @@ class Mail extends Component {
                         <FormItem>
                             <Button type="primary" htmlType="submit">Register</Button>
                         </FormItem>
+                            </div>
+                        </div>
                     </Form>
 
                 </div>
