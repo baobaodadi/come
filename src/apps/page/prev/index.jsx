0d7c7d66c34a5ec60769e17d1747979e88295461
shendadi <shendadi@sogou-inc.com>
2018-09-28 09:07:04 +0800

    cs

--- src/apps/page/prev/index.jsx
+++ src/apps/page/prev/index.jsx
@@ -7,6 +7,7 @@ import {Link, withRouter} from "react-router-dom";
 import * as actionTypes from "../../../config/actionTypes";
 import {connect} from "react-redux";
 import './index.less';
+import wei from '../../../images/2wei.png'
 import {
   Table,
   Input,
@@ -78,6 +79,37 @@ class Prev extends Component {
     return (
 
       <div className="prev">
+          <div className="toptitle">
+              <span className="mail">邮箱账号</span>
+              <span className="name">zhangsan@sogou-inc.com</span>
+          </div>
+          <div className="detail">
+              <div className="bigtitle">办公资产选择</div>
+              <div className="prev">
+                <span className='title'>预选资产</span>
+                <span className='bu'>补货中</span>
+                <span className='level'>笔记本电脑</span>
+                <span className='sword'>></span>
+                <span className='level'>DELL</span>
+              </div>
+              <div className="temp">
+                <span className='title'>临时资产</span>
+                <span className='level'>笔记本电脑</span>
+                <span className='sword'>></span>
+                <span className='level'>DELL</span>
+              </div>
+              <div className="default">
+                <span className='title'>默认耗材</span>
+                <span className='level'>笔记本电脑</span>
+                <span className='sword'>></span>
+                <span className='level'>DELL</span>
+              </div>
+          </div>
+          <div className="xiaop">
+              <div className="bigtitle">预装办公软件</div>
+              <img src={wei} alt=""/>
+              <div className="install">日常办公必备，请提前扫码安装</div>
+          </div>
         <div className="buttonclub">
           <Button type="primary" onClick={this.handleSubmit}>完成</Button>
           <Link to="/mail">
