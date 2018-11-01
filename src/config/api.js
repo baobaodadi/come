aeaa8221c468b43d0f6c2303783c3041b8306f0e
dadi <675834325@qq.com>
2018-11-01 12:06:45 +0800

    delete

--- src/config/api.js
+++
@@ -1,7 +0,0 @@
-import * as constants from './constants';
-
-export default {
-  [constants.ENTITY_ASSET]: '/pub/sylla/api/pub-rzzc/pre/info',
-  [constants.ENTITY_MAIL]: '/pub/sylla/api/pub-rzzc/set/mail',
-  [constants.ENTITY_CHANGE]: '/pub/sylla/api/pub-rzzc/set/asset',
-};
